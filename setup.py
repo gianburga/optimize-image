16769ba9338d8f8e05cff922bdd58fddf7bf607e
Franco Burga <franco.burga@gmail.com>
2019-01-31 16:35:47 -0500

    Fix optimize images vendor dir

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     url="https://github.com/francoburga/optimize-image",
     packages=setuptools.find_packages(),
     data_files=[
-        ('/opt/optimize-image/vendor/linux/', ['vendor/linux/cjpeg']),
+        ('/opt/optimize-images/vendor/linux/', ['vendor/linux/cjpeg']),
     ],
     install_requires=[
         'Pillow==5.4.1',
