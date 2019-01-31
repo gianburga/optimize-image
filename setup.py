f575aba7c0eca3bed6f7f6c354c46c3087948a50
Franco Burga <franco.burga@gmail.com>
2019-01-31 15:26:21 -0500

    Add requirements and setup configuration

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='optimize-image',  
+    version='0.1',
+    scripts=['optimize'] ,
+    author="Franco Burga",
+    author_email="franco.burga@gmail.com",
+    description="A image optimizer utility",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/francoburga/optimize-image",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'Pillow==5.4.1',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
