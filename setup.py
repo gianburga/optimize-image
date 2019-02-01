e0e5da6e7ebd9aaa588d40b029dab471121fd546
Franco Burga <franco.burga@gmail.com>
2019-02-01 15:16:04 -0500

    Update setup version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='optimize-image',  
-    version='0.1',
+    version='0.2',
     scripts=['optimize.py'] ,
     author="Franco Burga",
     author_email="franco.burga@gmail.com",
