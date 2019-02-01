27e6f771e3aabfe55346442534c996e40f121efa
Franco Burga <franco.burga@gmail.com>
2019-02-01 16:19:34 -0500

    Update optimize file

--- optimize.py
+++ optimize.py
@@ -9,6 +9,6 @@ logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
     quality = sys.argv[2] if len(sys.argv) >= 3 else 80
-    optimize_image = optimize_image(sys.argv[1], quality)
+    optimize_image = optimize_image(source_path=sys.argv[1], quality=quality)
     image = optimize_image['image']
     image.save('%s%s' % (optimize_image['filename'], optimize_image['extension']))
